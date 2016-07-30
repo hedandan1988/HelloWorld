99374a44044b443e51a51820c5c79dc38f4ca85a
it010 <it010@J007>
2016-07-30 16:43:32 +0800

    666

--- HelloWorld/src/HelloWorld/Controllers/BaseController.cs
+++ HelloWorld/src/HelloWorld/Controllers/BaseController.cs
@@ -8,6 +8,7 @@ using Model.Library;
 using System.Data.SqlClient;
 using Dapper;
 using Basic.Library;
+using Microsoft.AspNetCore.Mvc.Routing;
 
 // For more information on enabling MVC for empty projects, visit http://go.microsoft.com/fwlink/?LinkID=397860
 
@@ -15,6 +16,15 @@ namespace HelloWorld.Controllers
 {
     public class BaseController : Controller
     {
+        /// <summary>
+        /// 获得登录页对象
+        /// </summary>
+        /// <returns></returns>
+        protected RedirectResult GetAdminLoginResult()
+        {
+            return new RedirectResult("/admin/login");
+        }
+
         /// <summary>
         /// 当前管理员信息
         /// </summary>
@@ -30,7 +40,8 @@ namespace HelloWorld.Controllers
         /// <summary>
         /// 链接字符串
         /// </summary>
-        public const string CONNECTIONSTRING = @"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=HelloWorld;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=True;ApplicationIntent=ReadWrite;MultiSubnetFailover=False";
+        //public const string CONNECTIONSTRING = @"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=HelloWorld;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=True;ApplicationIntent=ReadWrite;MultiSubnetFailover=False";
+        public const string CONNECTIONSTRING = @"Data Source = (localdb)\MSSQLLocalDB;Initial Catalog = HelloWorld; Integrated Security = True; Connect Timeout = 30; Encrypt=False;TrustServerCertificate=True;ApplicationIntent=ReadWrite;MultiSubnetFailover=False";
         public override void OnActionExecuted(ActionExecutedContext context)
         {
             base.OnActionExecuted(context);
@@ -42,35 +53,39 @@ namespace HelloWorld.Controllers
             {
                 using (var conn = new SqlConnection(CONNECTIONSTRING))
                 {
-                    AdminInfo = conn.Query<Admin>("select top 1 * from admin where UserName=@UserName and UserPwd=@UserPwd",new { UserName=adminname,UserPwd=adminpwd}).FirstOrDefault();
+                    AdminInfo = conn.Query<Admin>("select top 1 * from admin where UserName=@UserName and UserPwd=@UserPwd", new { UserName = adminname, UserPwd = adminpwd }).FirstOrDefault();
                     if (AdminInfo == null || AdminInfo.Id == 0 || AdminInfo.Verify == (int)Status.AdminVerifyEnum.禁用)
                     {
-                        context.Result = new RedirectResult("/admin/login");
+                        context.Result = GetAdminLoginResult();
+                        context.Canceled = true;
                     }
                     //获取用户角色信息
                     SysRoleInfo = conn.Query<SysRole>("select top 1 * from SysRole where id=(select roleid from adminrole where adminid=@id and verify=@verify)", new { id = AdminInfo.Id, verify = (int)Status.SysRoleVerifyEnum.启用 }).FirstOrDefault();
                     if (SysRoleInfo == null || SysRoleInfo.Id == 0)
                     {
-                        context.Result = new RedirectResult("/admin/login");
+                        context.Result = GetAdminLoginResult();
                     }
                     //获取权限信息
                     SysAppList = conn.Query<SysApp>("select * from SysApp where id in(select appid from sysroleapp where verify=@verify and roleid=@id)", new { id = SysRoleInfo.Id, verify = (int)Status.SysAppVerifyEnum.启用 }).ToList();
                     //判断权限
                     string controller = context.RouteData.Values["controller"].ToString().ToLower();
-                    string action=context.RouteData.Values["action"].ToString().ToLower();
+                    string action = context.RouteData.Values["action"].ToString().ToLower();
                     SysApp app = SysAppList.FirstOrDefault(o => o.AppUrl.ToLower().Equals(controller + "/" + action));
-                    if (app==null)
+                    if (app == null)
                     {
-                        context.Result = new RedirectResult("/admin/login");
+                        context.Result = GetAdminLoginResult();
                     }
-                    ViewBag.AdminDataModel = new AdminDataModel {
-                         AdminInfo=AdminInfo, SysAppList=SysAppList, SysRoleInfo=SysRoleInfo
+                    ViewBag.AdminDataModel = new AdminDataModel
+                    {
+                        AdminInfo = AdminInfo,
+                        SysAppList = SysAppList,
+                        SysRoleInfo = SysRoleInfo
                     };
                 }
             }
             else
             {
-                context.Result = new RedirectResult("/admin/login");
+                context.Result = GetAdminLoginResult();
             }
 
         }

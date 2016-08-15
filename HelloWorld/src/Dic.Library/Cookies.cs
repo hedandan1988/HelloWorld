ae214a32dd928036935b5d6c03ba597d5283bfc0
it010 <it010@J007>
2016-08-15 09:13:32 +0800

    999

---
+++ HelloWorld/src/Dic.Library/Cookies.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Dic.Library
+{
+    /// <summary>
+    /// cookie名称
+    /// </summary>
+    public class Cookies
+    {
+        public const string 管理员账户 = "adminname";
+        public const string 管理员密码 = "adminpwd";
+        public const string 管理员ID = "adminid";
+    }
+}
--- HelloWorld/src/HelloWorld/Controllers/AdminController.cs
+++ HelloWorld/src/HelloWorld/Controllers/AdminController.cs
@@ -27,9 +27,9 @@ namespace HelloWorld.Controllers
             return View();
         }
         [HttpPost]
+        [ValidateAntiForgeryToken]
         public IActionResult Login(string username, string password)
         {
-
             if (username.IsNullOrEmpty() || password.IsNullOrEmpty())
             {
                 return Json(new { status = (int)Status.CommonStatusEnum.数据验证失败, msg = "用户名和者密码不能为空" });
@@ -40,8 +40,14 @@ namespace HelloWorld.Controllers
                 return Json(new { status = (int)Status.CommonStatusEnum.数据验证失败, msg = "用户名和者密码错误" });
             }
             //记录登录日志
+            var op =new Microsoft.AspNetCore.Http.CookieOptions();
+            op.Expires = new DateTimeOffset(DateTime.Now, new TimeSpan(1, 0, 0));
+            op.Domain = "/";
+            HttpContext.Response.Cookies.Append(Dic.Library.Cookies.管理员账户,admin.UserName,op);
+            HttpContext.Response.Cookies.Append(Dic.Library.Cookies.管理员密码, admin.UserPwd,op);
+            HttpContext.Response.Cookies.Append(Dic.Library.Cookies.管理员ID, admin.Id.ToString(),op);
             Tools.MSSQL.Execute("INSERT INTO AdminLoginLog VALUES(@Admin,GETDATE(),NULL,@LoginIP)", new {Admin=admin.Id,LoginIP=Request.Host });
-            throw new NotImplementedException();
+            return Json(new { status = (int)Status.CommonStatusEnum.成功, msg = "登陆成功" });
         }
     }
 }
